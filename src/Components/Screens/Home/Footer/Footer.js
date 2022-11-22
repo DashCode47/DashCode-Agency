59ebeff64b8ece0e33efab6b9e5096b495c16ac8
David Lozada <davsh47@hotmail.com>
2022-11-22 22:11:08 +0300

    end
    end

--- src/Components/Screens/Home/Footer/Footer.js
+++ src/Components/Screens/Home/Footer/Footer.js
@@ -6,7 +6,9 @@ const Footer = () => {
     <div className="footer-container">
       <div style={{ backgroundImage: `url(${bg})` }} className="footer-bg">
         <div className="footer-title">Contactanos:</div>
-        <div className="footer-mail">davsh47@hotmail.com</div>
+        <a className="footer-mail" href="mailto:davsh47@hotmail.com">
+          davsh47@hotmail.com
+        </a>
         <div className="footer-mail">(+7)9375764627</div>
         <div className="footer-copy">
           © COPYRIGHT 2022 DashCode AGENCIA DIGITAL
